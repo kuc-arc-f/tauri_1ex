ae2b29ac2e739adaa64ce37617a077cd818e185f
中島浩二 <knaka0209@gmail.com>
2025-01-12 19:27:02 +0900

    up, 0112a5

---
+++ tauri-nextjs/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+const Top: FC = async () => {
+  return (
+  <>
+    <h1 className="text-3xl font-bold">Hello!</h1>
+    <hr />
+    <p>tauri + nextjs 14</p>
+  </>
+
+  )
+}
+export default Top
